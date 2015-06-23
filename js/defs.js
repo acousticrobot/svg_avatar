0a2e3ab85ef141c377770891893e2756fafc94cd
Jonathan Gabel <hello@jonathangabel.com>
2015-06-23 18:08:30 -0400

    refactor component controller, add mouths

--- js/defs.js
+++ js/defs.js
@@ -3,35 +3,41 @@ var avatarGroups = {
     {
   "title": "invader",
   "shapes": [
-    {"type":"polygon","attr":{"class":"color-primary","points":"0,0 30,0 30,40 60,40 60,10 90,10 90,70 70,70 70,100 100,100 100,150 70,150 70,120 40,120 40,90 10,90 10,60 0,60 "}},
+    {"type":"polygon","attr":{"class":"color-primary","points":"0,0 30,0 30,40 60,40 60,10 90,10 90,70 70,70 70,100 100.397,100 100.397,150 70,150 70,120 40,120 40,90 10,90 10,60 0,60 "}},
     {"type":"rect","attr":{"class":"color-bright","x":"7","y":"3","width":"20","height":"54"}},
     {"type":"rect","attr":{"class":"color-bright","x":"67","y":"13","width":"20","height":"50"}},
     {"type":"rect","attr":{"class":"color-bright","x":"43","y":"45","width":"20","height":"50"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"13","y":"61","width":"20","height":"26"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"43","y":"97","width":"20","height":"20"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"73","y":"105","width":"20","height":"20"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"13","y":"61","width":"20","height":"26"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"43","y":"97","width":"20","height":"20"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"73","y":"105","width":"20","height":"20"}},
     {"type":"polygon","attr":{"class":"color-primary","points":"200.166,0 170.166,0 170.166,40 140.166,40 140.166,10 110.166,10 110.166,70 130.166,70 130.166,100 100.166,100 100.166,150 130.166,150 130.166,120 160.166,120 160.166,90 190.166,90 190.167,60 200.166,60 "}},
     {"type":"rect","attr":{"class":"color-bright","x":"173.166","y":"3","width":"20","height":"54"}},
     {"type":"rect","attr":{"class":"color-bright","x":"113.166","y":"13","width":"20","height":"50"}},
     {"type":"rect","attr":{"class":"color-bright","x":"137.166","y":"45","width":"20","height":"50"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"167.166","y":"61","width":"20","height":"26"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"137.166","y":"97","width":"20","height":"20"}},
-    {"type":"rect","attr":{"class":"color-dark","x":"107.166","y":"105","width":"20","height":"20"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"167.166","y":"61","width":"20","height":"26"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"137.166","y":"97","width":"20","height":"20"}},
+    {"type":"rect","attr":{"class":"color-primary","x":"107.166","y":"105","width":"20","height":"20"}},
     {"type":"rect","attr":{"class":"color-bright","x":"80","y":"127.367","width":"40","height":"20"}}
+  ]
+},
+    {
+  "title": "min",
+  "shapes": [
+
   ]
 },
     {
   "title": "retro",
   "shapes": [
-    {"type":"path","attr":{"class":"color-primary","d":"M104.001,40c-15.645-30.086,29.296-45.382,62-36c21.647,6.21,34,28.846,34,45c0,40.56,0,25.062,0,25.062s0.999,8.03,0,18.803C199.739,95.681,196,103,189,98c-12.711-9.079-14.07-37.058-31-51C141,33,130.001,90,104.001,40z"}},
-    {"type":"path","attr":{"class":"color-primary","d":"M59,100c0,22.091,41,40,41,40s41-17.909,41-40s-18.356-40-41-40S59,77.909,59,100z"}},
+    {"type":"path","attr":{"class":"color-dark","d":"M104.001,40c-15.645-30.086,29.296-45.382,62-36c21.647,6.21,34,28.846,34,45c0,40.56,0,25.062,0,25.062s0.999,8.03,0,18.803C199.739,95.681,196,103,189,98c-12.711-9.079-14.07-37.058-31-51C141,33,130.001,90,104.001,40z"}},
+    {"type":"path","attr":{"class":"color-dark","d":"M59,100c0,22.091,41,40,41,40s41-17.909,41-40s-18.356-40-41-40S59,77.909,59,100z"}},
     {"type":"polygon","attr":{"class":"color-primary","points":"106,53.78 198.455,9 111.083,20.103 "}},
     {"type":"polygon","attr":{"class":"color-primary","points":"108.669,83.292 184,45 117.848,49.485 "}},
     {"type":"polygon","attr":{"class":"color-primary","points":"120.621,105.014 183.379,78.986 131.929,77.403 "}},
     {"type":"circle","attr":{"class":"color-bright","cx":"114.5","cy":"24.5","r":"11.5"}},
     {"type":"circle","attr":{"class":"color-bright","cx":"115.5","cy":"54.5","r":"11.5"}},
     {"type":"circle","attr":{"class":"color-bright","cx":"135.5","cy":"78.5","r":"11.5"}},
-    {"type":"path","attr":{"class":"color-primary","d":"M96.444,38.748c15.645-30.086-29.296-45.382-62-36c-21.647,6.21-34,28.846-34,45c0,40.56,0,25.062,0,25.062s-0.999,8.03,0,18.803c0.261,2.816,4,10.136,11,5.136c12.711-9.079,14.07-37.058,31-51C59.444,31.748,70.444,88.748,96.444,38.748z"}},
+    {"type":"path","attr":{"class":"color-dark","d":"M96.444,38.748c15.645-30.086-29.296-45.382-62-36c-21.647,6.21-34,28.846-34,45c0,40.56,0,25.062,0,25.062s-0.999,8.03,0,18.803c0.261,2.816,4,10.136,11,5.136c12.711-9.079,14.07-37.058,31-51C59.444,31.748,70.444,88.748,96.444,38.748z"}},
     {"type":"polygon","attr":{"class":"color-primary","points":"94.444,52.528 1.99,7.748 89.362,18.851 "}},
     {"type":"polygon","attr":{"class":"color-primary","points":"91.776,82.04 16.444,43.748 82.597,48.232 "}},
     {"type":"polygon","attr":{"class":"color-primary","points":"79.824,103.761 17.065,77.734 68.516,76.15 "}},
@@ -46,38 +52,32 @@ var avatarGroups = {
     {
   "title": "infiniteyes",
   "shapes": [
-    {"type":"path","attr":{"class":"color-primary","d":"M90.001,21c0,0-20.147,21-45,21C20.148,42,0,21,0,21s20.147-21,45-21C69.854,0,90.001,21,90.001,21z"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"45","cy":"21","r":"21"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M31.888,13.121c-1.299-5.651,2.229-11.287,7.88-12.586s11.286,2.228,12.587,7.879C53.653,14.065,44.475,21,44.475,21S33.188,18.772,31.888,13.121z"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M34,31.5C34,25.701,44.5,21,44.5,21S55,25.701,55,31.5S50.299,42,44.5,42S34,37.299,34,31.5z"}},
-    {"type":"path","attr":{"class":"color-primary","d":"M100.75,21c0,0,20.147,21,45,21c24.853,0,45-21,45-21s-20.147-21-45-21C120.898,0,100.75,21,100.75,21z"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"145.751","cy":"21","r":"21"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M158.864,13.121c1.299-5.651-2.229-11.287-7.88-12.586s-11.286,2.228-12.587,7.879c-1.299,5.651,7.88,12.586,7.88,12.586S157.563,18.772,158.864,13.121z"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M156.751,31.5c0-5.799-10.5-10.5-10.5-10.5s-10.5,4.701-10.5,10.5s4.701,10.5,10.5,10.5S156.751,37.299,156.751,31.5z"}}
+    {"type":"path","attr":{"class":"color-primary","d":"M93.001,50c0,0-20.147,21-45,21S3,50,3,50s20.147-21,45-21C72.854,29,93.001,50,93.001,50z"}},
+    {"type":"circle","attr":{"class":"color-bright","cx":"48","cy":"50","r":"21"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M34.889,42.121c-1.299-5.651,2.229-11.287,7.88-12.586c5.651-1.299,11.286,2.228,12.587,7.879C56.653,43.065,47.476,50,47.476,50S36.188,47.772,34.889,42.121z"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M37,60.5C37,54.701,47.5,50,47.5,50S58,54.701,58,60.5S53.299,71,47.5,71S37,66.299,37,60.5z"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M107,50c0,0,20.147,21,45,21s45-21,45-21s-20.147-21-45-21C127.148,29,107,50,107,50z"}},
+    {"type":"circle","attr":{"class":"color-bright","cx":"152.001","cy":"50","r":"21"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M165.114,42.121c1.299-5.651-2.229-11.287-7.88-12.586c-5.651-1.299-11.286,2.228-12.587,7.879c-1.299,5.651,7.88,12.586,7.88,12.586S163.813,47.772,165.114,42.121z"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M163.001,60.5c0-5.799-10.5-10.5-10.5-10.5s-10.5,4.701-10.5,10.5s4.701,10.5,10.5,10.5S163.001,66.299,163.001,60.5z"}}
   ]
 },
     {
-  "title": "infiniteyes2",
+  "title": "min",
   "shapes": [
-    {"type":"path","attr":{"class":"color-primary","d":"M90.001,49c0,0-20.147,21-45,21C20.148,70,0,49,0,49s20.147-21,45-21C69.854,28,90.001,49,90.001,49z"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"45","cy":"49","r":"21"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M31.888,41.121c-1.299-5.651,2.229-11.287,7.88-12.586s11.286,2.228,12.587,7.879C53.653,42.065,44.475,49,44.475,49S33.188,46.772,31.888,41.121z"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M34,59.5C34,53.701,44.5,49,44.5,49S55,53.701,55,59.5S50.299,70,44.5,70S34,65.299,34,59.5z"}},
-    {"type":"path","attr":{"class":"color-primary","d":"M100.75,49c0,0,20.147,21,45,21c24.853,0,45-21,45-21s-20.147-21-45-21C120.898,28,100.75,49,100.75,49z"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"145.751","cy":"49","r":"21"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M158.864,41.121c1.299-5.651-2.229-11.287-7.88-12.586s-11.286,2.228-12.587,7.879c-1.299,5.651,7.88,12.586,7.88,12.586S157.563,46.772,158.864,41.121z"}},
-    {"type":"path","attr":{"class":"color-dark","d":"M156.751,59.5c0-5.799-10.5-10.5-10.5-10.5s-10.5,4.701-10.5,10.5s4.701,10.5,10.5,10.5S156.751,65.299,156.751,59.5z"}}
+    {"type":"circle","attr":{"class":"color-primary","cx":"40","cy":"50","r":"13"}},
+    {"type":"circle","attr":{"class":"color-primary","cx":"160","cy":"50","r":"13"}}
   ]
 },
     {
   "title": "zeitgeyes",
   "shapes": [
-    {"type":"path","attr":{"class":"color-primary","d":"m0,0l82,2-80,56h76z"}},
-    {"type":"path","attr":{"class":"color-primary","d":"m127,6l72-2-74,56h66z"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"40","cy":"14","r":"12"}},
-    {"type":"circle","attr":{"class":"color-dark","cx":"40","cy":"14","r":"6"}},
-    {"type":"circle","attr":{"class":"color-bright","cx":"160","cy":"18","r":"12"}},
-    {"type":"circle","attr":{"class":"color-dark","cx":"160","cy":"18","r":"6"}}
+    {"type":"path","attr":{"class":"color-primary","d":"M1,20l82,2L3,78h76L1,20z"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M128,26l72-2l-74,56h66L128,26z"}},
+    {"type":"circle","attr":{"class":"color-bright","cx":"41","cy":"34","r":"12"}},
+    {"type":"circle","attr":{"class":"color-primary","cx":"41","cy":"34","r":"6"}},
+    {"type":"circle","attr":{"class":"color-bright","cx":"161","cy":"38","r":"12"}},
+    {"type":"circle","attr":{"class":"color-primary","cx":"161","cy":"38","r":"6"}}
   ]
 }
   ],
@@ -102,6 +102,41 @@ var avatarGroups = {
   "title": "rolio",
   "shapes": [
     {"type":"circle","attr":{"class":"color-primary","cx":"100","cy":"100","r":"100"}}
+  ]
+}
+  ],
+  "mouths": [
+    {
+  "title": "min",
+  "shapes": [
+    {"type":"circle","attr":{"class":"color-primary","cx":"100","cy":"120.125","r":"13"}}
+  ]
+},
+    {
+  "title": "mums",
+  "shapes": [
+    {"type":"path","attr":{"class":"color-primary","d":"M76,65c2.846-0.949,19,5,24,5s19-9,25-7s12,14,1,21s-15,12-26,10S77,80,75,75S73,66,76,65z"}},
+    {"type":"path","attr":{"class":"color-primary","d":"M100,110c6.979,0.537,45,9,41,14s-20,5-31,5s-18-3-29,0s-21,4-17-3S87,109,100,110z"}},
+    {"type":"path","attr":{"class":"color-bright","d":"M101.882,11c4,0,13,4,18,17s10,40,12,45s-22,11-28,11s-23-5-26-9s-10-8-2-17s18-17,19-22s0-24,6-25"}}
+  ]
+},
+    {
+  "title": "porkley",
+  "shapes": [
+    {"type":"ellipse","attr":{"class":"color-primary","cx":"100","cy":"100","rx":"67","ry":"45"}},
+    {"type":"ellipse","attr":{"class":"color-primary","cx":"100","cy":"100","rx":"58","ry":"37"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"101","y":"66","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"121","y":"74","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"121","y":"108","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"141","y":"93","width":"14","height":"14"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"101","y":"116","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"81","y":"66","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"61","y":"74","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"61","y":"108","width":"18","height":"17"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"45","y":"93","width":"14","height":"14"}},
+    {"type":"rect","attr":{"class":"color-bright","x":"81","y":"116","width":"18","height":"17"}},
+    {"type":"circle","attr":{"class":"color-primary","cx":"85","cy":"35","r":"5"}},
+    {"type":"circle","attr":{"class":"color-primary","cx":"115","cy":"35","r":"5"}}
   ]
 }
   ]
--- js/main.js
+++ js/main.js
@@ -1,57 +1,49 @@
-var svg = d3.select("#avatar").append("svg").attr("width", 200).attr("height", 250);
+var svg = d3.select("#avatar").append("svg").attr("width", 200).attr("height", 300);
 
-var eyeGroups = _.pluck(avatarGroups.eyes,"title");
-var headGroups = _.pluck(avatarGroups.heads,"title");
-var crownGroups = _.pluck(avatarGroups.crowns,"title");
+var colorThemes = ["athens", "san-paulo", "miami", "phoenix"];
 
-var eyeCounter= 0;
-var headCounter = 0;
-var crownCounter = 0;
+// component = "eyes"
+var Component = function(component){
+  var type = component;
+  var maxGroups = avatarGroups[component].length - 1;
+  var groupCounter = _.random(maxGroups);
 
-function currentEyes() {
-  return avatarGroups.eyes[eyeCounter];
-}
-function currentHead() {
-  return avatarGroups.heads[headCounter];
-}
-function currentCrown() {
-  return avatarGroups.crowns[crownCounter];
-}
+  var maxThemes = colorThemes.length - 1;
+  var themeCounter = _.random(maxThemes);
 
-function nextComponent(componentCounter,componentGroup) {
-  if (componentCounter >= componentGroup.length - 1 ) {
-    componentCounter = 0;
-  } else {
-    componentCounter ++;
+  var group = function() {
+      return avatarGroups[component][groupCounter];
   }
-  console.log(componentCounter);
-  return componentCounter;
-}
-
-var colorThemes = ["athens", "san-paulo"];
-var eyeTheme = 0;
-var headTheme = 0;
-var crownTheme = 0;
-
-function currentEyeTheme() {
-  return colorThemes[eyeTheme];
-}
-function currentHeadTheme() {
-  return colorThemes[headTheme];
-}
-function currentCrownTheme() {
-  return colorThemes[crownTheme];
-}
-
-function nextTheme(componentTheme) {
-  console.log("sam i am");
-  if (componentTheme >= colorThemes.length - 1) {
-    componentTheme = 0;
-  } else {
-    componentTheme ++;
+  var theme = function() {
+    return colorThemes[themeCounter];
   }
-  return componentTheme;
+  var nextGroup = function() {
+    if (groupCounter >= maxGroups ) {
+      groupCounter = 0;
+    } else {
+      groupCounter ++;
+    }
+  }
+  var nextTheme = function() {
+    if (themeCounter >= maxThemes) {
+      themeCounter = 0;
+    } else {
+      themeCounter ++;
+    }
+  }
+  return {
+    type: type,
+    group: group,
+    theme: theme,
+    nextGroup: nextGroup,
+    nextTheme: nextTheme
+  }
 }
+
+var eyes = new Component("eyes");
+var head = new Component("heads");
+var crown = new Component("crowns");
+var mouth = new Component("mouths");
 
 function assembleShape(shape) {
   return function(){
@@ -63,75 +55,63 @@ function assembleShape(shape) {
   }
 }
 
-function assembleComponent(componentType,component,theme,x,y) {
+function assembleComponent(component,x,y) {
   return function(){
     var group = d3.select(document.createElementNS(d3.ns.prefix.svg, 'g'))
                   .attr("transform", "translate("+x+","+y+")")
-                  .attr("class",componentType + "-container " + theme);
-    for (var i=0; i < component.shapes.length; i++) {
-      var shape = assembleShape(component.shapes[i]);
+                  .attr("class",component.type + "-container " + component.theme());
+    for (var i=0; i < component.group().shapes.length; i++) {
+      var shape = assembleShape(component.group().shapes[i]);
       group.append(shape);
     }
     return group.node();
   }
 }
 
-
-// function assemble() {
-//   return function(){
-//     var group = d3.select(document.createElementNS(d3.ns.prefix.svg, 'g'))
-//                   .attr("class","avatar-container");
-//     for (var i=0; i < heads.shapes.length; i++) {
-
-//     }
-//     for (var i = 0; i < eyes.shapes.length; i++) {
-//       var shape =  assembleShape(eyes.shapes[i]);
-//       group.append(shape);
-//     }
-//     return group.node();
-//   }
-// }
-
-
-
-// initial build
 function build () {
   svg.selectAll("g").remove();
-  svg.append(assembleComponent("head",currentHead(),currentHeadTheme(),0,50));
-  svg.append(assembleComponent("crown",currentCrown(),currentCrownTheme(),0,0));
-  svg.append(assembleComponent("eyes",currentEyes(),currentEyeTheme(),0,100));
+  svg.append(assembleComponent(head,0,100));
+  svg.append(assembleComponent(crown,0,0));
+  svg.append(assembleComponent(eyes,0,100));
+  svg.append(assembleComponent(mouth,0,150));
 }
 
+// initial build
 build();
-
 
 // Wachers
 $("#crown-controller .theme").click(function(){
-  crownTheme = nextTheme(crownTheme);
+  crown.nextTheme();
   build();
 });
-
 $("#crown-controller .next").click(function(){
-  crownCounter = nextComponent(crownCounter,crownGroups);
+  crown.nextGroup();
   build();
 });
 
 $("#eye-controller .theme").click(function(){
-  eyeTheme = nextTheme(eyeTheme);
+  eyes.nextTheme();
   build();
 });
-
 $("#eye-controller .next").click(function(){
-  eyeCounter = nextComponent(eyeCounter,eyeGroups);
+  eyes.nextGroup();
   build();
 });
 
 $("#head-controller .theme").click(function(){
-  eyeTheme = nextTheme(eyeTheme);
+  head.nextTheme();
+  build();
+});
+$("#head-controller .next").click(function(){
+  head.nextGroup();
   build();
 });
 
-$("#head-controller .next").click(function(){
-  headCounter = nextComponent(headCounter,headGroups);
+$("#mouth-controller .theme").click(function(){
+  mouth.nextTheme();
+  build();
+});
+$("#mouth-controller .next").click(function(){
+  mouth.nextGroup();
   build();
 });
